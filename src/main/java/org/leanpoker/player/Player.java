66b2b7a86876b81d66409afe8da1176a31382101
Lorand Gyorgy-Fazakas <gyflorand@gmail.com>
2019-08-26 11:31:12 +0200

    Version 1.0.3

--- src/main/java/org/leanpoker/player/Player.java
+++ src/main/java/org/leanpoker/player/Player.java
@@ -7,16 +7,13 @@ import java.util.Map;
 
 public class Player {
 
-    static final String VERSION = "1.0.2";
+    static final String VERSION = "1.0.3";
 
     public static int betRequest(JsonElement request) {
 
-<<<<<<< HEAD
         JsonObject jsonObject = request.getAsJsonObject();
         return jsonObject.get("current_buy_in").getAsInt();
-=======
-        return 200;
->>>>>>> 67b7456fe2c8b01ba9d4f4bd05491b6312ea0965
+
     }
 
     public static void showdown(JsonElement game) {
