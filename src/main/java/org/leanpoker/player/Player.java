c4dbc2f772273fee0cb1ffb52c033131e966d5cc
Lorand Gyorgy-Fazakas <gyflorand@gmail.com>
2019-08-26 16:29:33 +0200

    las feature

--- src/main/java/org/leanpoker/player/Player.java
+++ src/main/java/org/leanpoker/player/Player.java
@@ -2,74 +2,77 @@ package org.leanpoker.player;
 
 import com.google.gson.JsonArray;
 import com.google.gson.JsonElement;
-import com.google.gson.JsonObject;
 
 import java.util.Arrays;
 
 public class Player {
 
     static final String VERSION = "1.0.5";
+    private static String rank1;
+    private static String rank2;
+    private static String suit1;
+    private static String suit2;
+
 
     public static int betRequest(JsonElement request) {
-
-        JsonObject jsonObject = request.getAsJsonObject();
 
         boolean bothCardsHigh = false;
         boolean bothCardsEqual = false;
         boolean bothCardsSuit = false;
         boolean checkPairs = false;
 
-        int playerId = jsonObject.get("in_action").getAsInt();
-        JsonArray jsonArray = jsonObject.get("players").getAsJsonArray();
-        for (JsonElement obj : jsonArray) {
-            if (obj.getAsJsonObject().get("id").getAsInt() == playerId) {
-                JsonArray myCards = obj.getAsJsonObject().get("hole_cards").getAsJsonArray();
+        String[] winner = {"10", "J", "Q", "K", "A"};
 
-                String rank1 = myCards.get(0).getAsJsonObject().get("rank").getAsString();
-                String rank2 = myCards.get(1).getAsJsonObject().get("rank").getAsString();
+        getMyCards(request);
 
-                String suit1 = myCards.get(0).getAsJsonObject().get("suit").getAsString();
-                String suit2 = myCards.get(1).getAsJsonObject().get("suit").getAsString();
-
-                String[] winner = {"10", "J", "Q", "K", "A"};
-
-                JsonArray communityCards = jsonObject.get("community_cards").getAsJsonArray();
-                for (JsonElement communityCard : communityCards) {
-                    System.out.println(communityCard);
-                    if ((communityCard.getAsJsonObject().get("rank").getAsString().equals(rank1) && Arrays.asList(winner).contains(rank1))
-                            || (Arrays.asList(winner).contains(rank2) && communityCard.getAsJsonObject().get("rank").getAsString().equals(rank2))) {
-                        checkPairs = true;
-                    }
-                }
-
-                if (rank1.equals(rank2)) {
-                    bothCardsEqual = true;
-                }
-
-                if (suit1.equals(suit2)) {
-                    bothCardsSuit = true;
-                }
-
-                if (Arrays.asList(winner).contains(rank1) && Arrays.asList(winner).contains(rank2)) {
-                    bothCardsHigh = true;
-                }
+        JsonArray communityCards = request.getAsJsonObject().get("community_cards").getAsJsonArray();
+        for (JsonElement communityCard : communityCards) {
+            System.out.println(communityCard);
+            if ((communityCard.getAsJsonObject().get("rank").getAsString().equals(rank1) && Arrays.asList(winner).contains(rank1))
+                    || (Arrays.asList(winner).contains(rank2) && communityCard.getAsJsonObject().get("rank").getAsString().equals(rank2))) {
+                checkPairs = true;
             }
+        }
+
+        if (rank1.equals(rank2)) {
+            bothCardsEqual = true;
+        }
+
+        if (suit1.equals(suit2)) {
+            bothCardsSuit = true;
+        }
+
+        if (Arrays.asList(winner).contains(rank1) && Arrays.asList(winner).contains(rank2)) {
+            bothCardsHigh = true;
         }
 
 
         if (bothCardsEqual || bothCardsHigh || checkPairs) {
-            return jsonObject.get("current_buy_in").getAsInt();
-        } else if (jsonObject.get("current_buy_in").getAsInt() == jsonObject.get("small_blind").getAsInt() * 2) {
-            return jsonObject.get("current_buy_in").getAsInt();
+            return request.getAsJsonObject().get("current_buy_in").getAsInt();
+        } else if (request.getAsJsonObject().get("current_buy_in").getAsInt() == request.getAsJsonObject().get("small_blind").getAsInt() * 2) {
+            return request.getAsJsonObject().get("current_buy_in").getAsInt();
         } else {
             return 0;
         }
 
-//        if (jsonObject.get("round").getAsInt() < 50)
-//            {return 0;}
-//        else
-//            {return jsonObject.get("current_buy_in").getAsInt();}
+    }
 
+    public static void getMyCards(JsonElement request) {
+
+        int playerId = request.getAsJsonObject().get("in_action").getAsInt();
+        JsonArray jsonArray = request.getAsJsonObject().get("players").getAsJsonArray();
+
+        for (JsonElement obj : jsonArray) {
+            if (obj.getAsJsonObject().get("id").getAsInt() == playerId) {
+                JsonArray myCards = obj.getAsJsonObject().get("hole_cards").getAsJsonArray();
+
+                rank1 = myCards.get(0).getAsJsonObject().get("rank").getAsString();
+                rank2 = myCards.get(1).getAsJsonObject().get("rank").getAsString();
+
+                suit1 = myCards.get(0).getAsJsonObject().get("suit").getAsString();
+                suit2 = myCards.get(1).getAsJsonObject().get("suit").getAsString();
+            }
+        }
     }
 
     public static void showdown(JsonElement game) {
