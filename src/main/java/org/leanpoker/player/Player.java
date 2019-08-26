cbfce410aa86c2cf1020b77b34b0d66c4e92ebd4
Lorand Gyorgy-Fazakas <gyflorand@gmail.com>
2019-08-26 13:23:48 +0200

    High Cards strategy

--- src/main/java/org/leanpoker/player/Player.java
+++ src/main/java/org/leanpoker/player/Player.java
@@ -1,8 +1,10 @@
 package org.leanpoker.player;
 
+import com.google.gson.JsonArray;
 import com.google.gson.JsonElement;
 import com.google.gson.JsonObject;
 
+import java.util.Arrays;
 import java.util.Map;
 
 public class Player {
@@ -13,13 +15,37 @@ public class Player {
 
         JsonObject jsonObject = request.getAsJsonObject();
 
-        if (jsonObject.get("round").getAsInt() < 50)
-            {return 0;}
-        else
-            {return jsonObject.get("current_buy_in").getAsInt();}
+        boolean bothCardsHigh = false;
+
+        int playerId = jsonObject.get("in_action").getAsInt();
+        JsonArray jsonArray = jsonObject.get("players").getAsJsonArray();
+        for (JsonElement obj : jsonArray) {
+            if (obj.getAsJsonObject().get("id").getAsInt() == playerId) {
+                JsonArray myCards = obj.getAsJsonObject().get("hole_cards").getAsJsonArray();
+
+                String rank1 = myCards.get(0).getAsJsonObject().get("rank").getAsString();
+                String rank2 = myCards.get(1).getAsJsonObject().get("rank").getAsString();
+                String[] winner = {"10", "J", "Q", "K", "A"};
+                if (Arrays.asList(winner).contains(rank1) && Arrays.asList(winner).contains(rank2) ) {
+                    bothCardsHigh = true;
+                }
+            }
+        }
+
+        if (bothCardsHigh) {
+             return jsonObject.get("current_buy_in").getAsInt();
+        } else {
+            return 0;
+        }
+
+//        if (jsonObject.get("round").getAsInt() < 50)
+//            {return 0;}
+//        else
+//            {return jsonObject.get("current_buy_in").getAsInt();}
 
     }
 
     public static void showdown(JsonElement game) {
     }
 }
+
