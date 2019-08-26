8cd977727bfc77ab80d5cfe8bb67feef133a4dd2
Attila Surányi <attila.suranyi@protonmail.com>
2019-08-26 10:37:45 +0200

    trying to stay in game

--- src/main/java/org/leanpoker/player/Player.java
+++ src/main/java/org/leanpoker/player/Player.java
@@ -9,7 +9,8 @@ public class Player {
     static final String VERSION = "1.0";
 
     public static int betRequest(JsonElement request) {
-        return 0;
+
+        return current_buy_in - players[in_action][bet];
     }
 
     public static void showdown(JsonElement game) {
